4c87440de7ac8f2f952c3a078c0163d862252395
Liam wong <liam_wong@appdet.com>
2020-10-30 14:07:00 +0800

    Create New Swift Flie

---
+++ v2.0_Mora/v2.0_Mora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  v2.0_Mora
+//
+//  Created by 1 on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

be2bbf202fa03825706e644104018c5cb2970a1c
Liam wong <liam_wong@appdet.com>
2020-11-02 15:30:46 +0800

    新增CellectionView and CellectionViewCell 畫面

---
+++ v2.0_Mora/v2.0_Mora/MoraCellectionView/MoraCellectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoraCellectionViewController.swift
+//  v2.0_Mora
+//
+//  Created by 1 on 2020/11/2.
+//
+
+import UIKit
+
+class MoraCellectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ v2.0_Mora/v2.0_Mora/MoraCellectionView/MoraCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MoraCollectionViewCell.swift
+//  v2.0_Mora
+//
+//  Created by 1 on 2020/11/2.
+//
+
+import UIKit
+
+class MoraCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
